157bdf3ec61cf51a6aeb49236382e25a88d3c131
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-08-15 20:13:18 +0100

    Update user.js

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -7,3 +7,20 @@ const {
   unFollow,
 } = require("../controllers/user.js");
 const { verifyToken } = require("../verifyToken.js");
+
+const router = express.Router();
+
+// Update User
+router.put("/:id", verifyToken, update);
+
+// Get User
+router.get("/find/:id", getUser);
+
+// Delete User
+router.delete("/:id", verifyToken, deleteUser);
+
+// Follow
+router.put("/follow/:id", verifyToken, follow);
+
+// Unfollow
+router.put("/unfollow/:id", verifyToken, unFollow);
