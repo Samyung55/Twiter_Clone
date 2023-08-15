7b8d299ebc7f5a6910773db06cbddb41272a46bd
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-08-15 20:16:50 +0100

    Update Tweet.js

--- api/models/Tweet.js
+++ api/models/Tweet.js
@@ -1,2 +1,20 @@
 const mongoose = require("mongoose");
 
+const TweetSchema = new mongoose.Schema(
+    {
+        userId: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+            max: 280,
+        },
+        likes: {
+            type: Array,
+            defaultValue: [],
+        },
+    },
+    { timestamps: true }
+)
