7610019a196160e7b45c0ca3dc0f690e606ac2ce
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-08-15 20:19:31 +0100

    Update tweets.js

---
+++ api/routes/tweets.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const { verifyToken } = require("../verifyToken.js");
+const {
+  createTweet,
+  deleteTweet,
+  likeOrDislike,
+  getAllTweets,
+  getUserTweets,
+  getExploreTweets,
+} = require("../controllers/tweet.js");
+
+const router = express.Router();
+
+// Create a Tweet
+router.post("/", verifyToken, createTweet);
+
+// Delete a Tweet
+router.delete("/:id", verifyToken, deleteTweet);
+
+// Like or Dislike a Tweet
+router.put("/:id/like", likeOrDislike);
+
+// get all timeline tweets
+router.get("/timeline/:id", getAllTweets);
+
+// get user Tweets only
+router.get("/user/all/:id", getUserTweets);
+
+//explore
+router.get("/explore", getExploreTweets);
+
+export default router;
