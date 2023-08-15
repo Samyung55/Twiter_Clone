06ddef48281686a166aa2f1b90ebf5f0ed63d325
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-08-15 20:17:55 +0100

    Update User.js

--- api/models/User.js
+++ api/models/User.js
@@ -7,5 +7,13 @@ const UserSchema = new mongoose.Schema(
         required: true,
         unique: true,
       },
-      
+      email: { type: String, required: true, unique: true },
+      password: { type: String, required: true },
+      profileProfile: { type: String },
+      followers: { type: Array, defaultValue: [] },
+      following: { type: Array, defaultValue: [] },
+      description: { type: String },
+      profilePicture: { type: String },
+    },
+    { timestamps: true }
   );
